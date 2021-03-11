73bb93e49af9eaa3fb2e743e8c43e22bf6a321ac
John Angel <usw4763@gmail.com>
2021-03-10 21:35:44 -0500

    Adding utility function to capitalize

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,3 +2,7 @@ export const getKeyValue = <U extends keyof T, T extends object>(
   key: U,
   obj: T,
 ) => obj[key];
+
+export const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
